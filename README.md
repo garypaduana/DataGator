5d3adacca29450ae900cdd3aa8614ff311089455
Gary Paduana <gary.paduana@gmail.com>
2014-10-10 22:44:15 -0400

    adding image/url scraper and cleaned up other integration components.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Spring Integration tool for common task automation including:
 - encode videos using HandbrakeCLI
 - rotate videos 90 deg clockwise using ffmpeg
 - encode mp3s using lame to lower filesize with lower variable bitrate
+- download jpg, gif, png, and webm by scraping an html document
 
 Dependencies for full functionality
 - youtube-dl
--- src/main/java/datagator/processor/FileProcessor.java
+++ src/main/java/datagator/processor/FileProcessor.java
@@ -1,11 +1,28 @@
 package datagator.processor;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
 
 public class FileProcessor {
 
@@ -26,6 +43,9 @@ public class FileProcessor {
 	private String sep = System.getProperty("file.separator");
 	private String rotateParent;
 	
+	private String scrapeRegex;
+	
+	private static final Logger log = Logger.getLogger(FileProcessor.class);
 	/**
 	 * Encode a file using HandBrakeCLI and a configurable preset encoding profile.
 	 * 
@@ -96,6 +116,14 @@ public class FileProcessor {
 		executeCommand(command);
 	}
 	
+	/**
+	 * Reencodes an mp3 to a lower variable bitrate.
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public File encodeMp3(File file) throws IOException, InterruptedException{
 		List<String> command = new ArrayList<String>();
 		new File(file.getParent() + "/output/").mkdirs();
@@ -137,6 +165,133 @@ public class FileProcessor {
 		return file;
 	}
 	
+	/**
+	 * The input file is a bookmark.  It should contain "URL=" followed by an 
+	 * Internet address pointing to an html file.  The contents of the file are
+	 * read and parsed looking for links to various media files.  Each link
+	 * is then downloaded to the current working directory.
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public File scrapeUrl(File file) throws IOException, URISyntaxException{
+		String contents = readFile(file);
+		String url = contents.split("URL=")[1];	
+		
+		String pageSource = getUrlContents(url);
+		
+		Matcher matcher = Pattern.compile(scrapeRegex).matcher(pageSource);
+		Set<String> links = new HashSet<String>();
+		while(matcher.find()){
+			links.add(matcher.group(1));
+		}
+		
+		for(String link : links){
+			log.debug("Downloading... " + link);
+			
+			File destination = new File(file.getParent(), link.substring(link.lastIndexOf("/") + 1, link.length()));
+			downloadFile(link, destination);
+		}
+		
+		return file;
+	}
+	
+	/**
+	 * Downloads the contents of a url to the specified destination.
+	 * 
+	 * @param url
+	 * @param destination
+	 * @throws IOException
+	 */
+	public void downloadFile(String url, File destination) throws IOException{
+		FileOutputStream fos = null;
+		URL site = null;
+		
+		try{
+			try{
+				site = new URL(url);
+			}
+			catch(MalformedURLException ex){
+				// 4chan currently specifies links as //i.4cdn.org/1234567890123.jpg
+				// this adds the protocol.  most other links from other sites are fully constructed.
+				site = new URL("http:" + url);
+			}
+			ReadableByteChannel rbc = Channels.newChannel(site.openStream());
+			fos = new FileOutputStream(destination);
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		}
+		finally{
+			if(fos != null){
+				fos.close();
+			}
+		}
+	}
+	
+	/**
+	 * Reads the contents of a File.
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 * @throws URISyntaxException
+	 */
+	public String readFile(File file) throws IOException, URISyntaxException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = null;
+		String line;
+		try{
+			br = new BufferedReader(new FileReader(file));
+			while((line = br.readLine()) != null){
+				sb.append(line);
+			}
+		}
+		finally{
+			if(br != null){
+				br.close();
+			}
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Reads the contents of a URL.
+	 * 
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public String getUrlContents(String url) throws IOException{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		URL site = new URL(url);
+		ReadableByteChannel rbc = Channels.newChannel(site.openStream());
+		WritableByteChannel out = Channels.newChannel(baos);
+		
+		copy(rbc, out);
+		
+		return new String(baos.toByteArray(), Charset.defaultCharset());
+	}
+	
+	/**
+	 * Read all available bytes from one channel and copy them to the other.
+	 * 
+	 * @param in
+	 * @param out
+	 * @throws IOException
+	 */
+	public static void copy(ReadableByteChannel in, WritableByteChannel out) throws IOException {
+		ByteBuffer buffer = ByteBuffer.allocateDirect(32 * 1024);
+		while (in.read(buffer) != -1) {
+			buffer.flip();
+			out.write(buffer);
+			buffer.compact();
+		}
+		buffer.flip();
+		while (buffer.hasRemaining()) {
+			out.write(buffer);
+		}
+	}
 		
 	/**
 	 * Executes a system process and prints InputStream and ErrorStream to
@@ -249,5 +404,13 @@ public class FileProcessor {
 
 	public void setRotateParent(String rotateParent) {
 		this.rotateParent = rotateParent;
+	}
+	
+	public String getScrapeRegex() {
+		return scrapeRegex;
+	}
+
+	public void setScrapeRegex(String scrapeRegex) {
+		this.scrapeRegex = scrapeRegex;
 	}
 }
